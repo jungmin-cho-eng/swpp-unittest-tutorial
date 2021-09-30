2f0ee0d4c608a04fd627679a262df1d52be7bb02
jungmin-cho-eng <jungmin97@snu.ac.kr>
2021-09-30 20:20:50 +0900

    Redux tests - TodoList, NewTodo test added

---
+++ src/setupTests.js
@@ -0,0 +1,8 @@
+// src/setupTests.js
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-17-updated';
+
+Enzyme.configure({
+    adapter: new EnzymeAdapter(),
+    disableLifecycleMethods: true
+})
